5d7708429bb5ba13989b7a2de7b9314f9dd9b509
quangcuong0808 <quangcuong0808@gmail.com>
2016-07-04 10:16:13 +0700

    add 2 screen

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+var publicPath = __dirname.slice(0,__dirname.length-6) + "public/";
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.type('text/html'); 
+	res.status(200); 
+	res.sendFile(publicPath + 'index.html');
+});
+
+router.get('/admin', function(req, res, next){
+	res.type('text/html'); 
+	res.status(200); 
+	res.sendFile(publicPath + 'admin.html');
 });
 
 module.exports = router;
